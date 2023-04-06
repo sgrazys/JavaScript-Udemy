0a4d3fe7e1c4807d0f22a0f6aacefaca5b3616b6
sgrazys <s.grazys@gmail.com>
2023-04-06 12:35:01 +0300

    spread operator

--- 05-Data-Structures-Operators/script.js
+++ 05-Data-Structures-Operators/script.js
@@ -40,6 +40,9 @@ const restaurant = {
   }
 };
 
+//////////////////////////////////
+//Spread Operator (...)
+
 const arr = [7, 8, 9]
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
