461ea5526c928ac86c9b853a5a8e7ff2d75de6a9
sgrazys <s.grazys@gmail.com>
2023-04-07 18:09:09 +0300

    Modern Logical assign Operators ||=  &&= ??=

--- 05-Data-Structures-Operators/script.js
+++ 05-Data-Structures-Operators/script.js
@@ -47,16 +47,47 @@ const restaurant = {
 
 };
 
+//////////////////////
+// LOGICAL ASSIGNMENT OPERATORS
+
+const rest1 = {
+  name: 'Capr1',
+  // numGuests: 20
+  numGuests: 0
+}
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Jean Luca'
+}
+
+// rest1.numGuests = rest1.numGuests || 10
+// rest2.numGuests = rest2.numGuests || 10
+
+
+// OR assignment operator
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// Nullish assignement operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// && assignment operator
+rest1.owner &&= '<ANONYMOUS>'
+rest2.owner &&= '<ANONYMOUS>'
+
+console.log(rest1);
+console.log(rest2);
 //Nulish coalescing operator ---> ??
 // Nulish values: null ir undetified (NOT 0 ar '' )
-restaurant.numGuests = 0;
+// restaurant.numGuests = null;
 
-const guests = restaurant.numGuests || 15
-console.log(guests);
+// const guests = restaurant.numGuests || 15
+// console.log(guests);
 
 
-const guestsCorrect = restaurant.numGuests ?? 15
-console.log(guestsCorrect);
+// const guestsCorrect = restaurant.numGuests ?? 15
+// console.log(guestsCorrect);
 
 // Logical operators can use ANY data type, return ANY data type, and they are used for short-cutting
 // console.log(`
