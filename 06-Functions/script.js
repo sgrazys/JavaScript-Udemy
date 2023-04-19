692d0137056c1822d66872ddff1b1eb3f3a866a6
sgrazys <s.grazys@gmail.com>
2023-04-19 14:49:54 +0300

    Daefault functions parameters

--- 06-Functions/script.js
+++ 06-Functions/script.js
@@ -1,5 +1,26 @@
 'use strict';
-
-
 ///////////////
 // DEFAULT PARAMETERS
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 399 * numPassengers) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 399
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price
+  }
+  console.log(booking);
+  bookings.push(booking,);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 798);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000)
