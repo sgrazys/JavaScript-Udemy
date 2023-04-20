8c3d6ff803bf27c7f9eba62503cec0204cb44b9b
sgrazys <s.grazys@gmail.com>
2023-04-20 11:56:48 +0300

    functions default parameters

--- 06-Functions/script.js
+++ 06-Functions/script.js
@@ -2,25 +2,51 @@
 ///////////////
 // DEFAULT PARAMETERS
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (flightNum, numPassengers = 1, price = 399 * numPassengers) {
-  // ES5
-  // numPassengers = numPassengers || 1;
-  // price = price || 399
+// const createBooking = function (flightNum, numPassengers = 1, price = 399 * numPassengers) {
+//   // ES5
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 399
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price
+//   }
+//   console.log(booking);
+//   bookings.push(booking,);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 798);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+
+// createBooking('LH123', undefined, 1000);
+
+///////////////
+// HOW PASSING ARGUMENTS WORKS: VALUE VS.REFERENCE
+
+const flight = 'LH123';
+const jokubas = {
+  name: 'Jokubas Grazys',
+  passport: 12634594123,
+  age: 6,
+}
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999',
+    passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 12634594123) {
+    alert('Check in')
+  } else {
+    alert('Wrong passport!')
   }
-  console.log(booking);
-  bookings.push(booking,);
-};
+}
 
-createBooking('LH123');
-createBooking('LH123', 2, 798);
-createBooking('LH123', 2);
-createBooking('LH123', 5);
+checkIn(flight, jokubas);
 
-createBooking('LH123', undefined, 1000)
+console.log(flight);
+console.log(jokubas);
