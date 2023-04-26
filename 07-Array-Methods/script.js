eace0f6960bb9e3ecea93555fb80e32e0e398f6e
sgrazys <s.grazys@gmail.com>
2023-04-26 23:26:12 +0300

    simple array methods

--- 07-Array-Methods/script.js
+++ 07-Array-Methods/script.js
@@ -2,67 +2,6 @@
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// BANKIST APP
-
-// Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
-
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
-
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
-
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
-
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // LECTURES
 
 const currencies = new Map([
@@ -75,3 +14,42 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+//SLICE - Grazina nauja value, NEMUTUOJA ORIGINALO
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+
+console.log(arr.slice(2)); // GRAZINA VISUS ELEMENTUS NUO 2-o indexo
+console.log(arr.slice(2, 4)); // GRAZINA NUO 2-o INDEXO IKI 3 (imtiniai) // 4-o NEITRAUKIA
+console.log(arr.slice(-1)); // GRAZINA PASKUTINI NARI
+console.log(arr.slice(-2)); // GRAZINA PASKUTINIUS 2 NARIUS
+console.log(arr.slice(1, -2)); // GRAZINA VISKA NUO 1-o INDEXO BE PASKUTINIU 2ju NARIU.
+console.log(arr.slice()); // MASYVO KOPIJA. REALIAI NAUDOJAMA KAI NORIME CHANININTI METODUS.
+console.log([...arr]) // KOPIJUOTI GALIMA IR NAUDOJANRT SPREAD OPERATORIU
+
+//SPLICE - GRAZINA ISSPLICENT'us ELEMENTUS !! MUTUOJA ORIGINALA!!
+// console.log(arr.splice(2)); // GRAZINS ELEMNTUS KURIUOS ISPLICE'inam
+arr.splice(-1); // ISTRINA PASKUTINI MASYVO ELEMENTA
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);  // MUTUOJA ORIGINALA GRAZINA ELEMENTUS KURIE NEBUVO ISSPLICE'inti.
+
+//REVERSE - REVERSINA (apsuka) ELEMENTUS MASYVE. !!MUTUOJA ORIGINALA
+arr = ['a', 'b', 'c', 'd', 'e'];
+let arr2 = ['j', 'i', 'h', 'g', 'f'];
+
+console.log(arr2) // ORIGINALUS MASYVAS;
+arr2.reverse() // APSUKA, REVERSINA MASYVO ELEMENTUS. 
+console.log(arr2); // GRAZINA MUTUOTA ORIGINALA (APVESRTA)
+
+
+// CONCAT - SUJUNGIU DU MASYVUS I VIENA. ORIGINALU NEMUTUOJA
+const letters = arr.concat(arr2)
+console.log(letters);
+
+// VEIKIA KAIP IR SPREAD OPERATORIUS
+console.log([...arr, ...arr2]);
+
+//JOIN - SUJUNGIA MASYVO ELEMENTUS SU NURODYTU 'sujungeju'.
+console.log(letters.join(' - '));
+console.log(letters.join('+'));
