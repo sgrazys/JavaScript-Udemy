3ca0602a6c96f97f115d540ff942c49e2493c711
sgrazys <s.grazys@gmail.com>
2023-05-02 15:41:29 +0300

    some() & ever()

--- 07-Array-Methods/script.js
+++ 07-Array-Methods/script.js
@@ -279,3 +279,25 @@ console.log(currentAcc);
 const index = accounts.findIndex(acc => acc.pin === currentAcc.pin);
 console.log(index);
 
+// // /////////////////////////////
+// // SOME & EVERY
+
+console.log(movements);
+//EQUALITY
+console.log(movements.includes(-130));
+
+//SOME: SPECIFY CONDITION
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+//EVERY
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+
+// Separate callback
+
+const deposit = mov => mov > 0
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
