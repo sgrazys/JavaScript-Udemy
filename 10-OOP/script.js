856a83ec0e461df651550d30bb7899bdf17ae71e
sgrazys <s.grazys@gmail.com>
2023-05-18 12:17:58 +0300

    ES6 Class

--- 10-OOP/script.js
+++ 10-OOP/script.js
@@ -2,74 +2,110 @@
 //////////////////
 // //CONSTRUCTOR FUNCTIONS AND THE NEW OPERATOR
 
-const Person = function (firstName, birthYear) {
-  // Instance properties
-  this.firstName = firstName;
-  this.birthYear = birthYear
+// const Person = function (firstName, birthYear) {
+//   // Instance properties
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  // NEVER TO DO THIS
-  // this.calcAge = function () {
-  //   console.log(2023 - this.birthYear);
-  // }
+//   // NEVER TO DO THIS
+//   // this.calcAge = function () {
+//   //   console.log(2023 - this.birthYear);
+//   // }
+// };
+
+// const jokubas = new Person('Jokubas', 2018);
+// console.log(jokubas);
+// // 1. New empty obj is created
+// // 2. function is called, and 'this' keyword point to new empty {}, this = {}
+// // 3. {} linked to prototype
+// // 4. function automatically return {}
+
+// const tauras = new Person('Tauras', 2021);
+// const marija = new Person('Marija', 1987);
+
+// console.log(tauras, marija);
+
+// // //Prototypes
+// console.log(Person.prototype);
+
+// Person.prototype.calcAge = function () {
+//   console.log(2023 - this.birthYear);
+// };
+
+// marija.calcAge();
+// jokubas.calcAge();
+// tauras.calcAge();
+
+// console.log(marija.__proto__);
+
+// console.log(Person.prototype.isPrototypeOf(marija));
+// console.log(Person.prototype.isPrototypeOf(tauras));
+// console.log(Person.prototype.isPrototypeOf(Person));
+
+// Person.prototype.species = 'Homo Sapiens';
+
+// console.log(marija, tauras);
+// console.log(marija.species, tauras.species);
+
+// console.log(marija.hasOwnProperty('firstName'));
+// console.log(marija.hasOwnProperty('species'));
+
+// // // Prototypal inheritance  on built-in objects
+// console.log(marija.__proto__);
+// // Object.prototype(top of prototype chain)
+// console.log(marija.__proto__.__proto__);
+// console.log(marija.__proto__.__proto__.__proto__);
+
+// console.dir(Person.prototype.constructor);
+
+// const arr = [3, 4, 5, 1, 4, 8, 3, 8, 3, 4, 5]; // new Array === []
+// console.log(arr.__proto__ === Array.prototype);
+// console.log(Array.prototype);
+
+// console.log(arr.__proto__.__proto__);
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.dir(x => x + 1);
+
+//////////////////
+// //ES CLASSES
+
+// class expression
+// const PersonCl = class {};
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  //Methods will be added to .prototype property
+  calcAge() {
+    console.log(2023 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey, ${this.firstName}`);
+  }
 }
 
-const jokubas = new Person('Jokubas', 2018);
-console.log(jokubas);
-// 1. New empty obj is created
-// 2. function is called, and 'this' keyword point to new empty {}, this = {}
-// 3. {} linked to prototype
-// 4. function automatically return {}
+const jenifer = new PersonCl('Jenifer', 1979);
+console.log(jenifer);
+jenifer.calcAge();
 
-const tauras = new Person('Tauras', 2021);
-const marija = new Person('Marija', 1987);
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey, ${this.firstName}`);
+// };
 
-console.log(tauras, marija);
+jenifer.greet();
 
-
-// //Prototypes
-console.log(Person.prototype);
-
-Person.prototype.calcAge = function () {
-  console.log(2023 - this.birthYear);
-};
-
-marija.calcAge();
-jokubas.calcAge();
-tauras.calcAge();
-
-console.log(marija.__proto__);
-
-console.log(Person.prototype.isPrototypeOf(marija));
-console.log(Person.prototype.isPrototypeOf(tauras));
-console.log(Person.prototype.isPrototypeOf(Person));
-
-Person.prototype.species = 'Homo Sapiens';
-
-console.log(marija, tauras);
-console.log(marija.species, tauras.species);
-
-console.log(marija.hasOwnProperty('firstName'));
-console.log(marija.hasOwnProperty('species'));
-
-// // Prototypal inheritance  on built-in objects
-console.log(marija.__proto__);
-// Object.prototype(top of prototype chain)
-console.log(marija.__proto__.__proto__);
-console.log(marija.__proto__.__proto__.__proto__);
-
-console.dir(Person.prototype.constructor);
-
-const arr = [3, 4, 5, 1, 4, 8, 3, 8, 3, 4, 5]; // new Array === []
-console.log(arr.__proto__ === Array.prototype);
-console.log(Array.prototype);
-
-console.log(arr.__proto__.__proto__);
-
-Array.prototype.unique = function () {
-  return [...new Set(this)]
-};
-
-console.log(arr.unique());
-
-const h1 = document.querySelector('h1');
-console.dir(x => x + 1)
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizens
+// 3. Classes are executed in strict mode
