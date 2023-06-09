697d0e0af8be88e60cb3524809ec6c736d0a10cc
sgrazys <s.grazys@gmail.com>
2023-06-09 13:58:11 +0300

    PROMISES AND FETCH API

--- 11-Asynchronous/script.js
+++ 11-Asynchronous/script.js
@@ -63,56 +63,89 @@ const renderCountry = function (data, className = '') {
 	countriesContainer.style.opacity = 1;
 };
 
-const getCountryDataAndNeighbour = function (country) {
-	// AJAX call country 1
-	const request = new XMLHttpRequest();
-	request.open('GET', `https://restcountries.com/v2/name/${country}`);
-	request.send();
+// const getCountryDataAndNeighbour = function (country) {
+// 	// AJAX call country 1
+// 	const request = new XMLHttpRequest();
+// 	request.open('GET', `https://restcountries.com/v2/name/${country}`);
+// 	request.send();
 
-	request.addEventListener('load', function () {
-		const [data] = JSON.parse(this.responseText);
-		console.log(data);
+// 	request.addEventListener('load', function () {
+// 		const [data] = JSON.parse(this.responseText);
+// 		console.log(data);
 
-		// render country 1
-		renderCountry(data);
+// 		// render country 1
+// 		renderCountry(data);
 
-		// Get neighbour country 2
-		const neighbour = data.borders?.[0];
-		if (!neighbour) return;
+// 		// Get neighbour country 2
+// 		const neighbour = data.borders?.[0];
+// 		if (!neighbour) return;
 
-		// AJAX call country 2
-		const request2 = new XMLHttpRequest();
-		request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
-		request2.send();
+// 		// AJAX call country 2
+// 		const request2 = new XMLHttpRequest();
+// 		request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+// 		request2.send();
 
-		request2.addEventListener('load', function () {
-			const data2 = JSON.parse(this.responseText);
-			console.log(data2);
+// 		request2.addEventListener('load', function () {
+// 			const data2 = JSON.parse(this.responseText);
+// 			console.log(data2);
 
-			renderCountry(data2, 'neighbour');
-		});
-	});
+// 			renderCountry(data2, 'neighbour');
+// 		});
+// 	});
+// };
+
+// // getCountryDataAndNeighbour('portugal');
+// getCountryDataAndNeighbour('portugal');
+
+// // CALLBACK HELLL
+// setTimeout(() => {
+// 	console.log('1 second passed');
+// 	setTimeout(() => {
+// 		console.log('2 seconds passed');
+// 		setTimeout(() => {
+// 			console.log('3 second passed');
+// 			setTimeout(() => {
+// 				console.log('4 second passed');
+// 				setTimeout(() => {
+// 					console.log('5 second passed');
+// 					setTimeout(() => {
+// 						console.log('6 second passed');
+// 					}, 1000);
+// 				}, 1000);
+// 			}, 1000);
+// 		}, 1000);
+// 	}, 1000);
+// }, 1000);
+
+//////////////////////////
+// PROMISES AND FETCH API
+
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v2/name/${country}`);
+// request.send();
+
+// const request = fetch('https://restcountries.com/v2/name/portugal');
+// console.log(request);
+
+// const getCountryData = function (country) {
+// 	fetch(`https://restcountries.com/v2/name/${country}`)
+// 		.then(function (response) {
+// 			console.log(response);
+// 			return response.json();
+// 		})
+// 		.then(function (data) {
+// 			console.log(data);
+// 			renderCountry(data[0]);
+// 		});
+// };
+
+// const request = fetch('https://restcountries.com/v2/name/portugal');
+// console.log(request);
+
+const getCountryData = function (country) {
+	fetch(`https://restcountries.com/v2/name/${country}`)
+		.then((response) => response.json())
+		.then((data) => renderCountry(data[0]));
 };
 
-// getCountryDataAndNeighbour('portugal');
-getCountryDataAndNeighbour('portugal');
-
-// CALLBACK HELLL
-setTimeout(() => {
-	console.log('1 second passed');
-	setTimeout(() => {
-		console.log('2 seconds passed');
-		setTimeout(() => {
-			console.log('3 second passed');
-			setTimeout(() => {
-				console.log('4 second passed');
-				setTimeout(() => {
-					console.log('5 second passed');
-					setTimeout(() => {
-						console.log('6 second passed');
-					}, 1000);
-				}, 1000);
-			}, 1000);
-		}, 1000);
-	}, 1000);
-}, 1000);
+getCountryData('lithuania');
