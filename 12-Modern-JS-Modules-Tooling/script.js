ea322eec35970988dba9cb1e1fb6d6bad0c9e393
sgrazys <s.grazys@gmail.com>
2023-06-12 11:49:53 +0300

    top level await

--- 12-Modern-JS-Modules-Tooling/script.js
+++ 12-Modern-JS-Modules-Tooling/script.js
@@ -21,3 +21,30 @@ add('watermelon', 12, 4);
 add('bannana', 122, 14);
 
 console.log(cart);
+
+////////////////////
+// TOP LEVEL-AWAIT
+
+// console.log('Start fetching');
+// const resp = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await resp.json();
+
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+	const resp = await fetch('https://jsonplaceholder.typicode.com/posts');
+	const data = await resp.json();
+	console.log(data);
+
+	return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then((last) => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
