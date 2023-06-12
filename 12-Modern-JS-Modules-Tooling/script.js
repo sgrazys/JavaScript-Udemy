e4c7661e27c0638ccb7496f15fb0a910a93026bd
sgrazys <s.grazys@gmail.com>
2023-06-12 10:13:56 +0300

    named imports and exports

---
+++ 12-Modern-JS-Modules-Tooling/script.js
@@ -0,0 +1,12 @@
+// Importing module
+
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq);
+// console.log('Importing module');
+// console.log(shippingCost);
+
+import * as ShoppingCart from './shoppingCart.js';
+
+ShoppingCart.addToCart('bread', 5);
+console.log(ShoppingCart.totalPrice);
