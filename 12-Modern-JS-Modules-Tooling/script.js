d9ed950e54e4f65d128cc6b0846e27fd1f7f52ad
sgrazys <s.grazys@gmail.com>
2023-06-12 10:57:27 +0300

    defaul exports

--- 12-Modern-JS-Modules-Tooling/script.js
+++ 12-Modern-JS-Modules-Tooling/script.js
@@ -3,10 +3,22 @@
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // addToCart('bread', 5);
 // console.log(price, tq);
-// console.log('Importing module');
+console.log('Importing module');
 // console.log(shippingCost);
 
-import * as ShoppingCart from './shoppingCart.js';
+// import * as ShoppingCart from './shoppingCart.js';
 
-ShoppingCart.addToCart('bread', 5);
-console.log(ShoppingCart.totalPrice);
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+import add, { cart, shippingCost } from './shoppingCart.js';
+add('pizza', 5, 2);
+add('bread', 15, 20);
+add('watermelon', 12, 4);
+add('bannana', 122, 14);
+
+console.log(cart);
+console.log(shippingCost);
--- 12-Modern-JS-Modules-Tooling/shoppingCart.js
+++ 12-Modern-JS-Modules-Tooling/shoppingCart.js
@@ -1,15 +1,46 @@
 // Exporting module
 console.log('Exporting module');
 
-const shippingCost = 10;
-const cart = [];
+export const shippingCost = 10;
+export const cart = [];
 
-export const addToCart = function (product, quantity) {
-	cart.push({ product, quantity });
-	console.log(`${quantity} ${product} was added to cart`);
+export const addToCart = function (product, quantity, price) {
+	cart.push({
+		product,
+		quantity,
+		price:
+			product === 'pizza'
+				? quantity * price
+				: product === 'bread'
+				? quantity * price
+				: quantity * price,
+	});
+	console.log(
+		`${quantity} ${product} was added to cart. Total of ${product} price: ${
+			price * quantity
+		}`
+	);
 };
 
 const totalPrice = 237;
 const totalQuantity = 23;
 
 export { totalPrice, totalQuantity as tq };
+
+export default function (product, quantity, price) {
+	cart.push({
+		product,
+		quantity,
+		price:
+			product === 'pizza'
+				? quantity * price
+				: product === 'bread'
+				? quantity * price
+				: quantity * price,
+	});
+	console.log(
+		`${quantity} ${product} was added to cart. Total of ${product} price: ${
+			price * quantity
+		}`
+	);
+}
